6a819eca1c629f3e01687c6bd6dd43b6eea0383c
Abhishekkumar1266 <samrat840@gmail.com>
2019-02-19 01:11:48 +0530

    Commit changes

---
+++ DemoNewProject/src/TestProject.java
@@ -0,0 +1,26 @@
+
+public class TestProject {
+
+	public static void main(String[] args) {
+		int a[]={2,5,7,8,4};
+		  int b[]={5,8,2,4,2};
+				int sum[]=new int[b.length];
+				int i;
+						for(i=0;i<a.length;i++){
+							sum[i]=0;
+							if(i>b.length){
+								sum[i]=b[i]+0;
+							}
+							else{
+					sum[i]=a[i]+b[i];
+				}
+						}
+						for(i=0;i<a.length;i++){
+				System.out.println(sum[i]);
+						}
+			}
+			
+
+	}
+
+
